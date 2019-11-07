f15ff6bb816d37d9f08a155abbf6ed2b4d9ba2df
Jenjo <longinuspun@gmail.com>
2019-11-07 04:10:59 -0800

    Convert tree building/heuristic to recursive

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -7,7 +7,7 @@ from BoardClasses import Board
 # Students can modify anything except the class name and exisiting functions and varibles.
 
 class Tree():
-    def __init__(self, color, move = None):
+    def __init__(self, color, move=None):
         self.color = color
         self.move = move
         self.value = None
@@ -32,7 +32,7 @@ class StudentAI():
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        #print("ALL MOVES: ", moves)
+        # print("ALL MOVES: ", moves)
 
         # #Random Moves
         # index = randint(0,len(moves)-1)#Inclusive of both bounds
@@ -70,110 +70,118 @@ class StudentAI():
                 self.board.undo()
         '''
 
-        #Start Tree
+        # # Start Tree
+        # root = Tree(self.opponent[self.color])  # Tree root
+        # for i in range(len(moves)):
+        #     for j in range(len(moves[i])):  # For each first move
+        #         root.children.append(Tree(self.color, moves[i][j]))  # Add children to root node
+        # '''
+        # Root
+        # Children
+        # '''
+        #
+        # l1_win = False
+        # l2_win = False
+        # win_move = None
+        #
+        # # Create Tree
+        # for l1_node in root.children:
+        #     # print("l1node")
+        #     self.board.make_move(l1_node.move, l1_node.color)
+        #     if (self.board.is_win(l1_node.color) == self.color):
+        #         l1_win = True
+        #         win_move = l1_node.move
+        #         break
+        #     l1_moves = self.board.get_all_possible_moves(self.opponent[l1_node.color])
+        #     # print(l1_node, "\n", l1_moves)
+        #     for i in range(len(l1_moves)):
+        #         for j in range(len(l1_moves[i])):
+        #             l1_node.children.append(Tree(self.opponent[l1_node.color], l1_moves[i][j]))
+        #             '''
+        #                 l0: Root
+        #                 l1: Children
+        #                 l2: ChildrenChildren
+        #             '''
+        #     for l2_node in l1_node.children:
+        #         self.board.make_move(l2_node.move, l2_node.color)
+        #         if (self.board.is_win(l2_node.color) == self.color):
+        #             l2_win = True
+        #             break
+        #         l2_moves = self.board.get_all_possible_moves(self.opponent[l2_node.color])
+        #         # print(l2_node, "\n", l2_moves)
+        #         for i in range(len(l2_moves)):
+        #             for j in range(len(l2_moves[i])):
+        #                 l2_node.children.append(Tree(self.opponent[l2_node.color], l2_moves[i][j]))
+        #                 '''
+        #                     l0: Root
+        #                     l1: Children
+        #                     l2: Children^2
+        #                     l3: Children^3
+        #                 '''
+        #         self.board.undo()
+        #     if l2_win:
+        #         win_move = l1_node.move
+        #         break
+        #     self.board.undo()
+        #
+        # if l1_win or l2_win:
+        #     cur_move = win_move
+        #
+        # else:
+        #     # Apply heuristic values for tree
+        #     for l1_node in root.children:
+        #         self.board.make_move(l1_node.move, l1_node.color)
+        #         for l2_node in l1_node.children:
+        #             self.board.make_move(l2_node.move, l2_node.color)
+        #             l2_moves = self.board.get_all_possible_moves(l1_node.color)
+        #             for l3_node in l2_node.children:
+        #                 self.board.make_move(l3_node.move, l3_node.color)
+        #                 l3_node.value = {self.board_points(): []}
+        #                 self.board.undo()
+        #
+        #             self.board.undo()
+        #             # print("l2_node_children")
+        #             l2_node.value = self.min_max(l2_node.children, l2_node.color)
+        #
+        #         self.board.undo()
+        #
+        #         # print("l1_node children")
+        #         l1_node.value = self.min_max(l1_node.children, l1_node.color)
+        #
+        #     # print("root children")
+        #     root.value = self.min_max(root.children, root.value)
+        #
+        #     # self.print_tree(root)
+        #
+        #     # root.value is dict that should only contain one entry
+        #     moves = root.value[list(root.value)[0]]
+        #     i = randint(0, len(moves) - 1)
+        #     cur_move = moves[i]
+
         root = Tree(self.opponent[self.color]) #Tree root
-        for i in range(len(moves)):
-            for j in range(len(moves[i])): #For each first move
-                root.children.append(Tree(self.color, moves[i][j])) #Add children to root node
-        '''
-        Root
-        Children
-        '''
+        self.rec_tree(root)
+        self.rec_heuristic(root)
 
-
-        l1_win = False
-        l2_win = False
-        win_move = None
-
-        #Create Tree
-        for l1_node in root.children:
-            #print("l1node")
-            self.board.make_move(l1_node.move, l1_node.color)
-            if (self.board.is_win(l1_node.color) == self.color):
-                l1_win = True
-                win_move = l1_node.move
-                break
-            l1_moves = self.board.get_all_possible_moves(self.opponent[l1_node.color])
-            #print(l1_node, "\n", l1_moves)
-            for i in range(len(l1_moves)):
-                for j in range(len(l1_moves[i])):
-                    l1_node.children.append(Tree(self.opponent[l1_node.color], l1_moves[i][j]))
-                    '''
-                        l0: Root
-                        l1: Children
-                        l2: ChildrenChildren
-                    '''
-            for l2_node in l1_node.children:
-                self.board.make_move(l2_node.move, l2_node.color)
-                if (self.board.is_win(l2_node.color) == self.color):
-                    l2_win = True
-                    break
-                l2_moves = self.board.get_all_possible_moves(self.opponent[l2_node.color])
-                #print(l2_node, "\n", l2_moves)
-                for i in range(len(l2_moves)):
-                    for j in range(len(l2_moves[i])):
-                        l2_node.children.append(Tree(self.opponent[l2_node.color], l2_moves[i][j]))
-                        '''
-                            l0: Root
-                            l1: Children
-                            l2: Children^2
-                            l3: Children^3
-                        '''
-                self.board.undo()
-            if l2_win:
-                win_move = l1_node.move
-                break
-            self.board.undo()
-
-        if l1_win or l2_win:
-            cur_move = win_move
-
-        else:
-            #Apply heuristic values for tree
-            for l1_node in root.children:
-                self.board.make_move(l1_node.move, l1_node.color)
-                for l2_node in l1_node.children:
-                    self.board.make_move(l2_node.move, l2_node.color)
-                    l2_moves = self.board.get_all_possible_moves(l1_node.color)
-                    for l3_node in l2_node.children:
-                        self.board.make_move(l3_node.move, l3_node.color)
-                        l3_node.value = {self.board_points(): []}
-                        self.board.undo()
-
-                    self.board.undo()
-                    #print("l2_node_children")
-                    l2_node.value = self.min_max(l2_node.children, l2_node.color)
-
-                self.board.undo()
-
-                #print("l1_node children")
-                l1_node.value = self.min_max(l1_node.children, l1_node.color)
-
-            #print("root children")
-            root.value = self.min_max(root.children, root.value)
-
-            #self.print_tree(root)
-
-            #root.value is dict that should only contain one entry
-            moves = root.value[list(root.value)[0]]
-            i = randint(0, len(moves) - 1)
-            cur_move = moves[i]
+        avail_moves = root.value[list(root.value)[0]]
+        print(avail_moves)
 
         self.board.make_move(cur_move, self.color)  # Make the optimal move
         move = cur_move
         return move
 
-    def min_max(self, children, color): #Returns dict -> {Max/min value: Moves to get here}
-        if color == self.color: #Calculate Min
-            ftu = min #Function to use
-        else: #Calculate Max
-            ftu = max #Function to use
+    def ftu(self, color): #Function to use (min vs max by color)
+        if color == self.color:  # Calculate Min
+            return min
+        else:  # Calculate Max
+            return max
 
+    def min_max(self, children, color):  # Returns dict -> {Max/min value: Moves to get here}
+        ftu = self.ftu(color) #Use corresponding min or max depending on color
         value_map = {}
         for child in children:
             for v in child.value.keys():
-                value_map.setdefault(v, []).append(child.move) #D: {heuristic value: Move to make to get here}
-        #print(value_map)
+                value_map.setdefault(v, []).append(child.move)  # D: {heuristic value: Move to make to get here}
+        # print(value_map)
         return {ftu(value_map): value_map[ftu(value_map)]}
 
     def board_points(self):  # 5 + row number for pawns, 5 + row number + 2 for kings
@@ -191,11 +199,52 @@ class StudentAI():
                         pts -= 2
         return pts if self.color == "B" else -pts
 
-    def print_tree(self, root, level = 0):
-        #print("PRINTING TREE")
+    def print_tree(self, root, level=0):
+        # print("PRINTING TREE")
 
         print("\t" * level, root.value, "->", root.move)
-        if len(root.children) != 0: #Not Leaf node
+        if len(root.children) != 0:  # Not Leaf node
             for child in root.children:
                 self.print_tree(child, level + 1)
 
+    def rec_tree(self, root: Tree, level=5):
+        if level == 0:
+            pass
+        else:
+            if root.move is not None:  # Not root of tree
+                self.board.make_move(root.move, root.color)
+            #Check if win here maybe?
+            avail_moves = self.board.get_all_possible_moves(self.opponent[root.color])
+            for i in range(len(avail_moves)):
+                for j in range(len(avail_moves[i])):
+                    #print(root)
+                    root.children.append( Tree(self.opponent[root.color], avail_moves[i][j] ) )
+            for child in root.children:
+                self.rec_tree(child, level - 1)
+
+            if root.move is not None:
+                self.board.undo()
+
+    def rec_heuristic(self, root: Tree):
+        if root.move is not None:
+            self.board.make_move(root.move, root.color)
+        if len(root.children) == 0: #Passed node has no children
+            pass #Evaluate heuristic for board(and return?)
+            root.value = {self.board_points(): []}
+        else: #Evaluate rec_heuristic for children, then retrieve values and apply min/max as appropriate
+            for child in root.children:
+                self.rec_heuristic(child)
+            root.value = self.min_max(root.children, root.color)
+
+        if root.move is not None:
+            self.board.undo()
+'''
+    def min_max2(self, children, color):  # Returns dict = {Max/min value: Moves to get here}
+        ftu = self.ftu(color)
+        value_map = {}
+        for child in children:
+            for v in child.value.keys():
+                value_map.setdefault(v, []).append(child.move)  # D: {heuristic value: Move to make to get here}
+
+        return {ftu(value_map): value_map[ftu(value_map)]}
+'''
